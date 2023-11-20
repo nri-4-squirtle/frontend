5002f0fe3b4cee89cbbd3efaf922b49e4697efaa
Matsusho0825 <sho.matsuda0825@gmail.com>
2023-11-20 16:37:38 +0900

    feat/showCurrentPosition #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
+import { useEffect, useState, useLayoutEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// import getCurrentPosition from './api/googleMapApi'
+
+import React, { useCallback, useRef } from 'react'
+import { GoogleMap, useLoadScript } from '@react-google-maps/api'
+
+const libraries = ['places']
+const mapContainerStyle = {
+  height: '100vh',
+  width: '100vh',
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [latitude, setLatitude] = useState(100)
+  const [longitude, setLongitude] = useState(100)
+
+  const mapRef = useRef()
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map
+  }, [])
+  //API読み込み後に再レンダーを引き起こさないため、useStateを使わず、useRefとuseCallbackを使っています。
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_googleMapsApiKey,
+    // ここにAPIキーを入力します。今回は.envに保存しています。
+    libraries,
+  })
+
+  useLayoutEffect(() => {
+    let pos = { lat: 0, lng: 0 }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude)
+          setLongitude(position.coords.longitude)
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter())
+        }
+      )
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter())
+    }
+  }, [])
+
+  if (loadError) return <div>Error</div>
+  if (!isLoaded) return <div>Loading</div>
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="googleMap">
+        <div className="transparent"></div>
+        <GoogleMap
+          id="map"
+          mapContainerStyle={mapContainerStyle}
+          zoom={18}
+          center={{
+            lat: latitude,
+            lng: longitude,
+          }}
+          onLoad={onMapLoad}
+        ></GoogleMap>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

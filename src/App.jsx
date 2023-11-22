1e122c43bcbe5623840829bb3d204ce903486eed
keigo <k31foot@gmail.com>
2023-11-22 09:03:07 +0900

    feat/getRestaurantInfo #4

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 // import getCurrentPosition from './api/googleMapApi'
 
 import React, { useCallback, useRef } from 'react'
-import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 
 // TODO : Performance warning
 const libraries = ['places']
@@ -15,73 +15,14 @@ const mapContainerStyle = {
   width: '100vh',
 }
 
-async function getNearFood(lat, lng) {
-  try {
-    if (
-      document.getElementById('map') == null ||
-      typeof document.getElementById('map') == null
-    ) {
-      return
-    }
-    var pyrmont = new google.maps.LatLng(
-      parseFloat(lat.toString()),
-      parseFloat(lng.toString())
-    )
-
-    const Map = new google.maps.Map(document?.getElementById('map'), {
-      center: pyrmont,
-      zoom: 18,
-    })
-
-    var request = {
-      location: pyrmont,
-      radius: 500,
-      type: 'restaurant',
-      keyword: '居酒屋', // 検索地点の付近を`keyword`を使って検索する
-    }
-
-    var service = new google.maps.places.PlacesService(Map)
-
-    // let result = []
-    // let stat
-
-    return service.nearbySearch(request, (res, status) => {
-      if (status != 'OK' && status != 'ZERO_RESULTS') {
-        console.log(lat, lng)
-        throw new Error(`status not OK ${status}`)
-      }
-
-      console.log(lat, lng)
-      console.log(status)
-      console.log('res = ' + res)
-
-      // stat = status
-      // result = res
-      // console.log('stat = ' + stat)
-      // console.log('result = ' + result)
-      // res.map((item) => {
-      //   console.log(item)
-      //   result.push({ name: item.name, place_id: item.place_id })
-      // })
-
-      // return res, status
-    })
-
-    // console.log('before return getNearFood')
-    // console.log(result)
-    // console.log(stat)
-    // return result
-  } catch (error) {
-    alert('検索処理でエラーが発生しました！')
-    throw error
-  }
-}
+let Map
+let infoWindows
 
 function App() {
   const [latitude, setLatitude] = useState(100)
   const [longitude, setLongitude] = useState(100)
-
-  // const [nearRest, setNearRest] = useState([])
+  //const [status, setStatus] = useState('')
+  //const [nearRest, setNearRest] = useState({})
 
   const mapRef = useRef()
   const onMapLoad = useCallback((map) => {
@@ -95,7 +36,100 @@ function App() {
     libraries,
   })
 
-  useLayoutEffect(() => {
+  //近くの飲食店の情報を取得する
+  function getNearFood(lat, lng) {
+    try {
+      if (
+        document.getElementById('map') == null ||
+        typeof document.getElementById('map') == null
+      ) {
+        return
+      }
+      //現在地の緯度経度を設定する
+      var currentPosition = new google.maps.LatLng(
+        parseFloat(lat.toString()),
+        parseFloat(lng.toString())
+      )
+      //現在地を表示するMAPを新規作成する
+      Map = new google.maps.Map(document?.getElementById('map'), {
+        center: currentPosition,
+        zoom: 18,
+      })
+      //現在地を示すマーカーを作成する
+      new google.maps.Marker({
+        icon: {
+          fillColor: '#115EC3', //塗り潰し色
+          fillOpacity: 0.8, //塗り潰し透過率
+          path: google.maps.SymbolPath.CIRCLE, //円を指定
+          scale: 10, //円のサイズ
+          strokeColor: '#FFFFFF', //枠の色
+          strokeWeight: 3,
+        },
+        position: { lat: lat, lng: lng },
+        Map,
+      })
+
+      //検索条件（近くの飲食店）を設定する
+      var request = {
+        location: currentPosition,
+        radius: 500,
+        type: 'restaurant',
+      }
+
+      //検索に必要なサービスを作成する
+      var service = new google.maps.places.PlacesService(Map)
+      //近くの飲食店を検索する
+      service.nearbySearch(request, cb)
+    } catch (error) {
+      alert('検索処理でエラーが発生しました！')
+      throw error
+    }
+  }
+  //nearbySerchの検索結果をマーカーを新規作成する処理に渡すcallback関数
+  function cb(res, status) {
+    if (status != 'OK' && status != 'ZERO_RESULTS') {
+      throw new Error(`status not OK ${status}`)
+    }
+    res.forEach((place) => {
+      console.log(place)
+      createMarker(place)
+    })
+    return
+  }
+  //マーカーを作成する
+  function createMarker(place) {
+    if (!place.geometry || !place.geometry.location) return
+    // お店情報マーカー
+    const marker = new google.maps.Marker({
+      map: Map,
+      position: place.geometry.location,
+      title: place.name,
+      optimized: false,
+    })
+
+    // お店情報ウィンドウ
+    infoWindows = new google.maps.InfoWindow()
+
+    // お店情報ウィンドウにて表示する情報
+    const infoList = [
+      place.name,
+      place.rating == undefined ? '評価：情報なし' : `評価：${place.rating}/5`,
+      place.photos && place.photos.length > 0
+        ? `<p><img style="max-width:200px" src="${place.photos[0].getUrl()}"/></p>`
+        : null,
+    ]
+
+    const info = infoList.join('<br>') // 改行区切りで加工して見せる
+    google.maps.event.addListener(marker, 'click', () => {
+      if (infoWindows == undefined || infoWindows == null) return
+      infoWindows.close()
+      infoWindows.setContent(info)
+      infoWindows.open(Map, marker)
+    })
+  }
+
+  //現在地の情報を取得する
+  function setLatLng() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -109,24 +143,18 @@ function App() {
     } else {
       handleLocationError(false, infoWindow, map.getCenter())
     }
+  }
+
+  useLayoutEffect(() => {
+    setLatLng()
   }, [])
 
+  //近くの飲食店の情報を取得し、マーカーを立てる
   useEffect(() => {
-    const func = async () => {
-      // TODO : ここで map id 要素が null でなくなるのを待つ.
-      setTimeout(() => {
-        console.log('In App Components')
-
-        getNearFood(latitude, longitude)
-
-        // console.log(status)
-        //if (status == 'OK') {
-        // console.log(result)
-        //}
-      }, 1000)
-    }
-
-    func()
+    // TODO : ここで map id 要素が null でなくなるのを待つ.
+    setTimeout(() => {
+      getNearFood(latitude, longitude)
+    }, 1000)
   }, [latitude])
 
   if (loadError) return <div>Error</div>
@@ -145,8 +173,16 @@ function App() {
             lng: longitude,
           }}
           onLoad={onMapLoad}
-        >
-          <MarkerF
+        ></GoogleMap>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+//memo
+/*           <MarkerF
             icon={{
               fillColor: '#115EC3', //塗り潰し色
               fillOpacity: 0.8, //塗り潰し透過率
@@ -159,11 +195,4 @@ function App() {
               lat: latitude,
               lng: longitude,
             }}
-          />
-        </GoogleMap>
-      </div>
-    </>
-  )
-}
-
-export default App
+          /> */

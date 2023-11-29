bfa9cf2663ddb305a321ea0958112691c00a33e3
本田真暉 <hondamakotoakira@hondamakotoakiranoMacBook-Pro.local>
2023-11-29 09:50:45 +0900

    Add Header and UI Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState, useLayoutEffect } from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { createRoot } from 'react-dom/client'
+
 import './App.css'
 import { getIconInfoList } from './api/parkingAreaApi'
 
@@ -7,7 +10,9 @@ import { GoogleMap, useLoadScript } from '@react-google-maps/api'
 
 import InfoWindowContent from './components/InfoWindowContent'
 import ReviewFormComponent from './components/ReviewFormComponent'
-import ReactDOMServer from 'react-dom/server'
+import Header from './components/Header'
+import SwitchButton from './components/SwitchButton'
+import CurrentFocusButton from './components/CurrentFocusButton'
 
 import Modal from 'react-modal'
 import ModalComponent from './components/ModalComponent'
@@ -15,7 +20,7 @@ import ModalComponent from './components/ModalComponent'
 // TODO : Performance warning
 const libraries = ['places']
 const mapContainerStyle = {
-  height: '100vh',
+  height: '90vh',
   width: '100%',
 }
 
@@ -47,6 +52,18 @@ function App() {
     // ここにAPIキーを入力します。今回は.envに保存しています。
     libraries,
   })
+
+  // 口コミなしのマーカーの表示を切り替える
+  function switchShowMarker(showAllRest) {
+    markerList.forEach((marker) => {
+      marker.setVisible(
+        showAllRest
+          ? parkInfoGlobal[marker.getTitle()].carNum != '0' &&
+              parkInfoGlobal[marker.getTitle()].carNum != null
+          : true
+      )
+    })
+  }
 
   //近くの飲食店の情報を取得する
   function getNearFood(lat, lng) {
@@ -92,21 +109,35 @@ function App() {
       var service = new google.maps.places.PlacesService(Map)
       //近くの飲食店を検索する
       service.nearbySearch(request, cb)
+
+      // 画面表示の編集（不要なGoogleMapのボタンを非表示、ボタンの追加
+      Map.setOptions({ mapTypeControl: false });
+      Map.setOptions({ fullscreenControl: false });
+      Map.setOptions({ streetViewControl: false });
+      Map.setOptions({ zoomControl: false });
+
+      // Create SwitchButton and CurrentFocusButton
+      const switchButtonDiv = document.createElement("div");
+      const switchRoot = createRoot(switchButtonDiv);
+      switchRoot.render(
+        <SwitchButton 
+          switchShowMarker={switchShowMarker}
+          showAllRest={showAllRest}
+          changeShowState={(isState) => setShowAllRest(isState)}
+          />);
+      Map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(switchButtonDiv);      //現在地を示すマーカーを作成する
+
+      const currentFocusButtonDiv = document.createElement("div");
+      const currentFocusRoot = createRoot(currentFocusButtonDiv);
+      currentFocusRoot.render(<CurrentFocusButton setLatLng={setLatLng}/>);
+      Map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(currentFocusButtonDiv);      //現在地を示すマーカーを作成する
+
     } catch (error) {
       alert('検索処理でエラーが発生しました！')
       throw error
     }
   }
-  function switchShowMarker(showAllRest) {
-    markerList.forEach((marker) => {
-      marker.setVisible(
-        showAllRest
-          ? parkInfoGlobal[marker.getTitle()].carNum != '0' &&
-              parkInfoGlobal[marker.getTitle()].carNum != null
-          : true
-      )
-    })
-  }
+
   /**
    * @description 検索結果をマーカーを新規作成する処理に渡すcallback関数
    * @Input res 検索結果 status 検索結果のステータス
@@ -158,8 +189,7 @@ function App() {
       visible: parkInfo.carNum != '0' && parkInfo.carNum != null,
       label: {
         className: 'markerLabel',
-        text:
-          place.name.length > 8 ? place.name.slice(0, 8) + '...' : place.name,
+        text: place.name.length > 8 ? place.name.slice(0, 8) + '...' : place.name,
       },
       icon: {
         url: `https://maps.google.com/mapfiles/kml/paddle/${
@@ -250,7 +280,11 @@ function App() {
 
   return (
     <>
-      <div className="googleMap">
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <div className="header" style={{ flex: '0 0 65px', overflow: 'hidden' }}>
+          <Header />
+        </div>
+        <div className="googleMap" style={{ flex: '1', overflow: 'auto' }}>
         <GoogleMap
           id="map"
           mapContainerStyle={mapContainerStyle}
@@ -267,14 +301,7 @@ function App() {
         curentClickedPlace={curentClickedPlace}
         onClose={() => handleDetailVisible()}
       />
-      <input
-        type="button"
-        value="Switch"
-        onClick={() => {
-          switchShowMarker(showAllRest)
-          setShowAllRest(!showAllRest)
-        }}
-      />
+      </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { AppBar, Container, Toolbar,} from '@mui/material';
+import icon from './icon.png';
+
+// ここまで変更
+
+const Header = () => {
+
+  return (
+    <AppBar position="sticky" color="default">
+      <Container maxWidth="xl">
+        {/* MUIのAppbar上に、左寄せでアイコン画像の表示させたい */}
+        <Toolbar disableGutters>
+            {/* ここから変更 */}
+            <img src={icon} alt="icon" style={{ maxHeight: "50px", padding:"5px"}}/>
+            {/* ここまで変更 */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
